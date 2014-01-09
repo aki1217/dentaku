62325a648f90813dda4323ca9708bbb0561ae6f1
Akihiro Fujii <aki_insane_liquor@yahoo.co.jp>
2014-01-09 16:01:27 +0900

    電卓できた

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -10,4 +10,50 @@
 
 @interface ViewController : UIViewController
 
+@property(nonatomic, assign)NSInteger buttonNumber;
+
+@property(nonatomic, assign)NSInteger countNumber;
+
+@property (weak,nonatomic) IBOutlet UILabel *numberOutput;
+ -(IBAction)imputNumber1:(id)sender;
+- (IBAction)imputNumber2:(id)sender;
+- (IBAction)imputNumber3:(id)sender;
+- (IBAction)imputNumber4:(id)sender;
+- (IBAction)imputNumber5:(id)sender;
+- (IBAction)imputNumber6:(id)sender;
+- (IBAction)imputNumber7:(id)sender;
+- (IBAction)imputNumber8:(id)sender;
+- (IBAction)imputNumber9:(id)sender;
+- (IBAction)equal:(id)sender;
+- (IBAction)plus:(id)sender;
+- (IBAction)mainasu:(id)sender;
+- (IBAction)kakeru:(id)sender;
+- (IBAction)waru:(id)sender;
+- (IBAction)cansel:(id)sender;
+- (IBAction)puramai:(id)sender;
+- (IBAction)imputNumber0:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber0;
+
+@property (weak, nonatomic) IBOutlet UIButton *cansel;
+@property (weak, nonatomic) IBOutlet UIButton *puramai;
+@property (weak, nonatomic) IBOutlet UIButton *waru;
+
+@property (weak, nonatomic) IBOutlet UIButton *kakeru;
+
+@property (weak, nonatomic) IBOutlet UIButton *mainasu;
+@property (weak, nonatomic) IBOutlet UIButton *plus;
+@property (weak, nonatomic) IBOutlet UIButton *equal;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber9;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber8;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber7;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber6;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber5;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber4;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber3;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber2;
+@property (weak, nonatomic) IBOutlet UIButton *imputNumber1;
+
+
+
+-(void)configureView;
 @end
--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -8,11 +8,20 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    double x;
+    double y;
+    int z;
+    int a;
+}
 
 @end
 
 @implementation ViewController
+
+@synthesize buttonNumber;
+
+@synthesize countNumber;
 
 - (void)viewDidLoad
 {
@@ -26,4 +35,138 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)imputNumber1:(id)sender{
+    x = x * 10 + 1;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+
+}
+
+- (IBAction)imputNumber2:(id)sender {
+    x = x * 10 + 2;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)imputNumber3:(id)sender {
+    x = x * 10 + 3;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)imputNumber4:(id)sender {
+    x = x * 10 + 4;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];}
+
+- (IBAction)imputNumber5:(id)sender {
+    x = x * 10 + 5;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];}
+
+- (IBAction)imputNumber6:(id)sender {
+    x = x * 10 + 6;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)imputNumber7:(id)sender {
+    x = x * 10 + 7;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)imputNumber8:(id)sender {
+    x = x * 10 + 8;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)imputNumber9:(id)sender {
+    x = x * 10 + 9;
+    a = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)equal:(id)sender {
+    switch(z){
+        case 1:
+            x = y + x;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+            break;
+            
+        case 2:
+    x = y - x;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+            break;
+            
+        case 3:
+            x = x * y;
+            self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+            break;
+            
+        case 4:
+            x = y / x;
+            self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+            break;
+    
+    }
+}
+
+- (IBAction)plus:(id)sender {
+    y = x;
+    x = 0;
+    z = 1;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)mainasu:(id)sender {
+    y = x;
+    x = 0;
+    z = 2;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)kakeru:(id)sender {
+    y = x;
+    x = 0;
+    z = 3;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];}
+
+- (IBAction)waru:(id)sender {
+    y = x;
+    x = 0;
+    z = 4;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)cansel:(id)sender {
+    x = 0;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+    
+}
+
+- (IBAction)puramai:(id)sender {
+    y = -x;
+    x = y;
+    self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+}
+
+- (IBAction)imputNumber0:(id)sender {
+    switch (a) {
+        case 1:
+            x = 0;
+             self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];        case  2:
+            x = x-1 * 10 + 10;
+            self.numberOutput.text = [NSString stringWithFormat:@"%.f",x];
+    }
+        
+    
+}
+
+-(void)labelOutput{
+
+    
+
+}
 @end
